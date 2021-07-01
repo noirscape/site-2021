b94191b4394d44522a8025165d1f2fa65c75d108
L. Stingray <neko@catgirlsin.space>
2021-07-01 22:18:47 +0200

    Update deploy script to restart services

--- release.sh
+++ release.sh
@@ -1,2 +1,4 @@
 cargo build --release --target x86_64-unknown-linux-musl
+ssh hetzner systemctl stop techpriestsite
 scp target/x86_64-unknown-linux-musl/release/techpriestsite hetzner:/opt/noirscape.dev/techpriestsite
+ssh hetzner systemctl start techpriestsite
